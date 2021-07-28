e1e1fb1f5199ef0c0e4265e8ac4a1359ea079281
BogdanKalysh <bogdankalysh20@gmail.com>
2021-07-28 10:37:45 +0300

    added logic for calculator

--- app/src/main/java/com/example/mycalculator/Calculator.java
+++ app/src/main/java/com/example/mycalculator/Calculator.java
@@ -1,7 +1,100 @@
 package com.example.mycalculator;
 
+import android.view.View;
+
 public class Calculator {
+    public enum calcState{
+        FIRST_ARG_INPUT,
+        SECOND_ARG_INPUT,
+        SHOW_RESULT
+    }
 
+    private int firstArg, secondArg;
+    private int actionId;
+    StringBuilder input = new StringBuilder();
+    calcState state;
 
+    public Calculator() {
+        state = calcState.FIRST_ARG_INPUT;
+    }
 
+    public String getText() {
+        return input.toString();
+    }
+
+    public void onNumPressed(int id) {
+        if(input.length() < 9) {
+
+            if(state == calcState.SHOW_RESULT){
+                input.setLength(0);
+                state = calcState.FIRST_ARG_INPUT;
+            }
+
+            switch(id){
+                case R.id.zero:
+                    if(input.length() > 0)
+                        input.append("0");
+                    break;
+                case R.id.one:
+                    input.append("1");
+                    break;
+                case R.id.two:
+                    input.append("2");
+                    break;
+                case R.id.three:
+                    input.append("3");
+                    break;
+                case R.id.four:
+                    input.append("4");
+                    break;
+                case R.id.five:
+                    input.append("5");
+                    break;
+                case R.id.six:
+                    input.append("6");
+                    break;
+                case R.id.seven:
+                    input.append("7");
+                    break;
+                case R.id.eight:
+                    input.append("8");
+                    break;
+                case R.id.nine:
+                    input.append("9");
+                    break;
+            }
+        }
+    }
+
+    public void onActionPressed(int id) {
+        if (id == R.id.equals && input.length() > 0) {
+            secondArg = Integer.parseInt(input.toString());
+            input.setLength(0);
+            state = calcState.SHOW_RESULT;
+
+            switch (actionId) {
+                case R.id.plus:
+                    input.append(firstArg + secondArg);
+                    break;
+                case R.id.minus:
+                    input.append(firstArg - secondArg);
+                    break;
+                case R.id.multiply:
+                    input.append(firstArg * secondArg);
+                    break;
+                case R.id.divide:
+                    input.append(firstArg / secondArg);
+                    break;
+            }
+        } else if (id == R.id.clear) {
+                input.setLength(0);
+                state = calcState.FIRST_ARG_INPUT;
+                actionId = 0;
+        } else if (input.length() > 0 && state == calcState.FIRST_ARG_INPUT) {
+                actionId = id;
+                firstArg = Integer.parseInt(input.toString());
+                input.setLength(0);
+                state = calcState.SECOND_ARG_INPUT;
+        }
+    }
 }
--- app/src/main/java/com/example/mycalculator/MainActivity.java
+++ app/src/main/java/com/example/mycalculator/MainActivity.java
@@ -3,12 +3,64 @@ package com.example.mycalculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Calculator calculator;
+    private TextView inputField;
+
+    private int[] numButtonsId = {
+            R.id.zero,
+            R.id.one,
+            R.id.two,
+            R.id.three,
+            R.id.four,
+            R.id.five,
+            R.id.six,
+            R.id.seven,
+            R.id.eight,
+            R.id.nine
+    };
+
+    private int[] actionButtonsId = {
+            R.id.plus,
+            R.id.minus,
+            R.id.multiply,
+            R.id.divide,
+            R.id.equals,
+            R.id.clear
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        calculator = new Calculator();
+        inputField = findViewById(R.id.numbersField);
+
+        View.OnClickListener numberButtonClickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                calculator.onNumPressed(v.getId());
+                inputField.setText(calculator.getText());
+            }
+        };
+
+        View.OnClickListener actionButtonClickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                calculator.onActionPressed(v.getId());
+                inputField.setText(calculator.getText());
+            }
+        };
+
+        for(int id : numButtonsId)
+            findViewById(id).setOnClickListener(numberButtonClickListener);
+
+        for(int id : actionButtonsId)
+            findViewById(id).setOnClickListener(actionButtonClickListener);
     }
 }
