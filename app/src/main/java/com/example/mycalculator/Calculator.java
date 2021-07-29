1a48304ffd08ad0c822e28ac5de9b91a02402678
BogdanKalysh <bogdankalysh20@gmail.com>
2021-07-29 11:28:33 +0300

    Refactored calculator class and removed android libraries, remade functionality

--- app/src/main/java/com/example/mycalculator/Calculator.java
+++ app/src/main/java/com/example/mycalculator/Calculator.java
@@ -2,99 +2,112 @@ package com.example.mycalculator;
 
 import android.view.View;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Calculator {
-    public enum calcState{
+
+    private enum CalcState{
         FIRST_ARG_INPUT,
         SECOND_ARG_INPUT,
         SHOW_RESULT
     }
+    private final ArrayList<Character> actionSymbols = new ArrayList<Character>(
+            Arrays.asList('+','-','*','/')
+    );
 
-    private int firstArg, secondArg;
-    private int actionId;
-    StringBuilder input = new StringBuilder();
-    calcState state;
+    private int firstArg, secondArg, result;
+    private char action;
+    CalcState state;
 
     public Calculator() {
-        state = calcState.FIRST_ARG_INPUT;
+        state = CalcState.FIRST_ARG_INPUT;
+        action = ' ';
     }
 
-    public String getText() {
-        return input.toString();
-    }
+    public String getResult() {
 
-    public void onNumPressed(int id) {
-        if(input.length() < 9) {
+        System.out.println("\n\n" + Integer.toString(firstArg) + " ----- " + Integer.toString(secondArg) + "\n\n\n");
 
-            if(state == calcState.SHOW_RESULT){
-                input.setLength(0);
-                state = calcState.FIRST_ARG_INPUT;
+        if (state == CalcState.FIRST_ARG_INPUT) {
+            return "";
+        } else if (state == CalcState.SECOND_ARG_INPUT) {
+            switch (action) {
+                case '+':
+                    return Integer.toString(firstArg + secondArg);
+                case '-':
+                    return Integer.toString(firstArg - secondArg);
+                case '*':
+                    return Integer.toString(firstArg * secondArg);
+                case '/':
+                    if (secondArg == 0)
+                        return "";
+                    return Integer.toString(firstArg / secondArg);
             }
-
-            switch(id){
-                case R.id.main_activity__zero:
-                    if(input.length() > 0)
-                        input.append("0");
-                    break;
-                case R.id.main_activity__one:
-                    input.append("1");
-                    break;
-                case R.id.main_activity__two:
-                    input.append("2");
-                    break;
-                case R.id.main_activity__three:
-                    input.append("3");
-                    break;
-                case R.id.main_activity__four:
-                    input.append("4");
-                    break;
-                case R.id.main_activity__five:
-                    input.append("5");
-                    break;
-                case R.id.main_activity__six:
-                    input.append("6");
-                    break;
-                case R.id.main_activity__seven:
-                    input.append("7");
-                    break;
-                case R.id.main_activity__eight:
-                    input.append("8");
-                    break;
-                case R.id.main_activity__nine:
-                    input.append("9");
-                    break;
-            }
+        } else {
+            return Integer.toString(result);
         }
+
+        return "Error";
     }
 
-    public void onActionPressed(int id) {
-        if (id == R.id.main_activity__equals && input.length() > 0) {
-            secondArg = Integer.parseInt(input.toString());
-            input.setLength(0);
-            state = calcState.SHOW_RESULT;
+    public boolean addNumber(int num) {
+        int lim = 999999999;
 
-            switch (actionId) {
-                case R.id.main_activity__plus:
-                    input.append(firstArg + secondArg);
+        if (state == CalcState.FIRST_ARG_INPUT && firstArg < lim && (firstArg != 0 || num != 0)) {
+            firstArg = (firstArg * 10) + num;
+            return true;
+        } else if (state == CalcState.SECOND_ARG_INPUT && secondArg < lim && (secondArg != 0 || num != 0)) {
+            secondArg = (secondArg * 10) + num;
+            return true;
+        } else if (state == CalcState.SHOW_RESULT) {
+            firstArg = num;
+            return true;
+        }
+
+        return false;
+    }
+
+    public boolean addAction(char act) {
+
+        if(act == 'C') { //reset calculator
+            firstArg = secondArg = 0;
+            action = ' ';
+            state = CalcState.FIRST_ARG_INPUT;
+            return true;
+        } else if (state == CalcState.FIRST_ARG_INPUT && actionSymbols.contains(act)) { //entering first arg and adding action
+            action = act;
+            state = CalcState.SECOND_ARG_INPUT;
+            return true;
+        }  else if (state == CalcState.SECOND_ARG_INPUT && act == '=') { //entering second arg and want result
+            state = CalcState.SHOW_RESULT;
+            switch (action) {
+                case '+':
+                    result = firstArg + secondArg;
                     break;
-                case R.id.main_activity__minus:
-                    input.append(firstArg - secondArg);
+                case '-':
+                    result = firstArg - secondArg;
                     break;
-                case R.id.main_activity__multiply:
-                    input.append(firstArg * secondArg);
+                case '*':
+                    result = firstArg * secondArg;
                     break;
-                case R.id.main_activity__divide:
-                    input.append(firstArg / secondArg);
+                case '/':
+                    if (secondArg == 0)
+                        return false;
+                    result = firstArg / secondArg;
                     break;
             }
-        } else if (id == R.id.main_activity__resetbutton) {
-                input.setLength(0);
-                state = calcState.FIRST_ARG_INPUT;
-                actionId = 0;
-        } else if (input.length() > 0 && state == calcState.FIRST_ARG_INPUT) {
-                actionId = id;
-                firstArg = Integer.parseInt(input.toString());
-                input.setLength(0);
-                state = calcState.SECOND_ARG_INPUT;
+            firstArg = secondArg = 0;
+            action = ' ';
+            return true;
+        } else if (state == CalcState.SHOW_RESULT && actionSymbols.contains(act)) { // got result and want to work with it
+            firstArg = result;
+            action = act;
+            state = CalcState.SECOND_ARG_INPUT;
+            return true;
         }
+
+        return false;
+
     }
 }
--- app/src/main/java/com/example/mycalculator/MainActivity.java
+++ app/src/main/java/com/example/mycalculator/MainActivity.java
@@ -6,32 +6,38 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class MainActivity extends AppCompatActivity {
 
     private Calculator calculator;
-    private TextView inputField;
+    private TextView inputField, resultField;
 
-    private int[] numButtonsId = {
-            R.id.main_activity__zero,
-            R.id.main_activity__one,
-            R.id.main_activity__two,
-            R.id.main_activity__three,
-            R.id.main_activity__four,
-            R.id.main_activity__five,
-            R.id.main_activity__six,
-            R.id.main_activity__seven,
-            R.id.main_activity__eight,
-            R.id.main_activity__nine
-    };
+    private ArrayList<Integer> numButtonsId = new ArrayList<Integer>(
+            Arrays.asList( R.id.main_activity__zero,
+                    R.id.main_activity__one,
+                    R.id.main_activity__two,
+                    R.id.main_activity__three,
+                    R.id.main_activity__four,
+                    R.id.main_activity__five,
+                    R.id.main_activity__six,
+                    R.id.main_activity__seven,
+                    R.id.main_activity__eight,
+                    R.id.main_activity__nine)
+    );
+    private ArrayList<Integer> actionButtonsId = new ArrayList<Integer>(
+            Arrays.asList(R.id.main_activity__plus,
+                    R.id.main_activity__minus,
+                    R.id.main_activity__multiply,
+                    R.id.main_activity__divide,
+                    R.id.main_activity__equals,
+                    R.id.main_activity__resetbutton)
+    );
+    private ArrayList<Character> actionSymbols = new ArrayList<Character>(
+            Arrays.asList('+','-','*','/','=','C')
+    );
 
-    private int[] actionButtonsId = {
-            R.id.main_activity__plus,
-            R.id.main_activity__minus,
-            R.id.main_activity__multiply,
-            R.id.main_activity__divide,
-            R.id.main_activity__equals,
-            R.id.main_activity__resetbutton
-    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,20 +46,36 @@ public class MainActivity extends AppCompatActivity {
 
         calculator = new Calculator();
         inputField = findViewById(R.id.main_activity__textfield);
+        resultField = findViewById(R.id.main_activity__result);
 
         View.OnClickListener numberButtonClickListener = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                calculator.onNumPressed(v.getId());
-                inputField.setText(calculator.getText());
+                int number = numButtonsId.indexOf(v.getId());
+
+                if (calculator.addNumber(number)) {
+                    inputField.append(Integer.toString(number));
+                    resultField.setText(calculator.getResult());
+                }
             }
         };
 
         View.OnClickListener actionButtonClickListener = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                calculator.onActionPressed(v.getId());
-                inputField.setText(calculator.getText());
+                char action = actionSymbols.get(actionButtonsId.indexOf(v.getId()));
+
+                if (calculator.addAction(action)) {
+                    inputField.append(Character.toString(action));
+                    resultField.setText(calculator.getResult());
+                    if(action == '=') {
+                        inputField.setText(calculator.getResult());
+                        resultField.setText("");
+                    } else if(action == 'C') {
+                        inputField.setText("");
+                        resultField.setText("");
+                    }
+                }
             }
         };
 
